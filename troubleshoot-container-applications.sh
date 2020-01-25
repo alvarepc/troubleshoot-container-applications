9df4e1dbde8b415b2d21af5613b6e8819a78ada7
alvarepc <55838813+alvarepc@users.noreply.github.com>
2020-01-25 18:13:57 +0800

    Update troubleshoot-container-applications.sh

--- troubleshoot-container-applications.sh
+++ troubleshoot-container-applications.sh
@@ -19,6 +19,7 @@ printf "%s\n" "-----------------------------------------------------------------
 printf "WARNING: Do not press any key while the script running\n"
 cp /etc/sysconfig/docker /etc/sysconfig/docker.lab.bkp
 cp ./docker /etc/sysconfig/docker
+dos2unix /etc/sysconfig/docker
 systemctl restart docker
 printf "%s${NC}\n" "------------------------------------------------------------------------------"
 printf "%s%s\n" "$var: Logging in as the developer user :$var"
